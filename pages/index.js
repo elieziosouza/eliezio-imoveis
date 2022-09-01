d24f57e9e47de1328ea15267046f82ff41f239ee
Eliezio <83556540+elieziosouza@users.noreply.github.com>
2022-08-31 23:13:47 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Bem Vindo ao meu!" />
+        <Header title="Bem Vindo ao meu App!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
